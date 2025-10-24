e592e0c19f5ccd54a7d511a76e6ca26d89c65a7e
往世的落霞 <github@rakkasu.me>
2025-10-24 18:10:04 +0800

    Update manifest.py

--- scripts/manifest.py
+++ scripts/manifest.py
@@ -30,7 +30,7 @@ def generate(filename, version, csproj) -> dict:
         'checksum': md5sum(filename),
         'changelog': 'Auto Released by Actions',
         'targetAbi': f'{get_jellyfin_version(csproj)}.0',
-        'sourceUrl': 'https://github.com/metatube-community/jellyfin-plugin-metatube/releases/download/'
+        'sourceUrl': 'https://github.com/rakkasumi/jellyfin-plugin-metatube/releases/download/'
                      f'v{version}/Jellyfin.MetaTube@v{version}.zip',
         'timestamp': datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
         'version': version
@@ -47,7 +47,7 @@ def main() -> None:
                           "../Jellyfin.Plugin.MetaTube/Jellyfin.Plugin.MetaTube.csproj")
 
     with urlopen(
-            'https://raw.githubusercontent.com/metatube-community/jellyfin-plugin-metatube/dist/manifest.json') as f:
+            'https://raw.githubusercontent.com/rakkasumi/jellyfin-plugin-metatube/dist/manifest.json') as f:
         manifest = json.load(f)
 
     manifest[0]['versions'].insert(0, generate(filename, version, csproj))
